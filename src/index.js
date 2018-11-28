e3bc3a161ba28ca227d107ca97a8ed91c3039738
joontop <joontop@linecorp.com>
2018-11-28 13:58:17 +0900

    sample add

--- src/index.js
+++ src/index.js
@@ -1,52 +1,42 @@
-import CircleChart from './CircleChart/CircleChart';
+import CircleChart from 'joontop-circlechart';
 
 let start = function() {
   let options = {
     data: [
       {
-        color: '#336699',
-        html: '<div class="test_contents">Love</div><p class="percent">9%</p>',
-        percent: 9,
+        color: '#777fc6', // 색상
+        html: '<div class="coin">Ethereum<br>30%</div>', // 보여줄 텍스트를 HTML로
+        percent: 30, // 합이 100 이 되도록
       },
       {
-        color: '#4477aa',
-        html: '<div class="test_contents">Passion</div>',
-        percent: 16,
-      },
-      {
-        color: '#5588bb',
-        html: '<div class="test_contents">Envy</div>',
+        color: '#82bce0',
+        html: '<div class="coin">Bitcoin<br>25%</div>',
         percent: 25,
       },
       {
-        color: '#6699cc',
-        html: '<div class="test_contents">Good</div>',
-        percent: 32,
+        color: '#7ed794',
+        html: '<div class="coin">Bitcoin Cash<br>22%</div>',
+        percent: 22,
       },
       {
-        color: '#77aadd',
-        html: '<div class="test_contents">Good</div>',
-        percent: 8,
+        color: '#fbb34e',
+        html: '',
+        percent: 13,
       },
       {
-        color: '#88bbee',
-        html: '<div class="test_contents">Bad</div>',
-        percent: 5,
-      },
-      {
-        color: '#99ccff',
-        html: '<div class="test_contents">Sad</div>',
-        percent: 5,
+        color: '#444444',
+        html: '',
+        percent: 10,
       },
     ],
-    outerDiameter: 300,
-    innerDiameter: 150,
-    outerColor: '#99ccff',
-    innerColor: '#ffffff',
-    isContents: true,
-    isDesc: true,
-    contentsMinWidth: 150,
-    target: document.querySelector('#donutTest'),
+    outerDiameter: 234, // 실제 도넛의 width,height
+    innerDiameter: 200, // 안쪽원의 크기,  0이면 없음
+    outerColor: '#99ccff', // 도넛의 기본 색상
+    innerColor: '#ffffff', // 안쪽원의 색상
+    isContents: true, // 텍스트의 유무
+    isDesc: true, // 가장 큰 percent 부터 시계방향 표시
+    contentsMinWidth: 150, // 표시되는 텍스트영역의 최소값
+    target: document.querySelector('#donutTest'), // chart 가 들어갈 영역지정
   };
   const circleChart = new CircleChart(options);
   circleChart.start();
